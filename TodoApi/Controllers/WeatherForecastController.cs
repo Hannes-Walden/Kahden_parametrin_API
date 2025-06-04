edab75a4c6ba0f152c72b988002842b5edd0f446
Hannes-Walden <hannes.walden@outlook.com>
2025-06-04 11:27:23 +0300

    Remove Weather related code.
    Remove default template.

--- TodoApi/Controllers/WeatherForecastController.cs
+++ TodoApi/Controllers/WeatherForecastController.cs
@@ -3,30 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace TodoApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+[Route("api")]
+public class twoThingsController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    [HttpGet(Name = "Kahden_parametrin_API")]
+    public IEnumerable<int> Get()
     {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return Enumerable.Range(1, 5).ToArray();
     }
 }
