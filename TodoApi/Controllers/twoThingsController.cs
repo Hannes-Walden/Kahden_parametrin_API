33c1940d5927629c9263e7203d0ac36073a82bb0
Hannes-Walden <hannes.walden@outlook.com>
2025-06-04 13:39:23 +0300

    Add new endpoint
    ...which takes two parameters.

--- TodoApi/Controllers/twoThingsController.cs
+++ TodoApi/Controllers/twoThingsController.cs
@@ -10,6 +10,11 @@ public class twoThingsController : ControllerBase
     [HttpGet(Name = "Kahden_parametrin_API")]
     public IEnumerable<int> Get()
     {
-        return Enumerable.Range(1, 5).ToArray();
+        return Enumerable.Range(1, 5000).ToArray();
+    }
+
+    public IEnumerable<int> Get(int p1, int p2)
+    {
+        return Enumerable.Range(p1, p2).ToArray();
     }
 }
